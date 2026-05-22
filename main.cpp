9d85a3d465732bb12bc6b524b0c0619c49125282
LEE JUNWON <leejunwon03@kookmin.ac.kr>
2026-05-23 01:29:25 +0900

    항목 추가로 인한 스코어보드 크기 조절
    Increased the height of win3 for the scoreboard.

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
 
   win1 = newwin(30, 30, 1, 2);
   win2 = newwin(12, 20, 1, 35);
-  win3 = newwin(12, 20, 14, 35);
+  win3 = newwin(13, 20, 14, 35); // 스코어보드 크기 늘림
 
   refresh();
   game();
