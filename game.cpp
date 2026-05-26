5341c0fdf73f9459a2bfa9d46a387163a86fc625
LEE JUNWON <leejunwon03@kookmin.ac.kr>
2026-05-26 17:28:31 +0900

    Feat: first refactoring

--- game.cpp
+++ game.cpp
@@ -1,66 +1,58 @@
-#include "window.cpp"
-#include "gate.cpp"
-#include "snake.cpp"
-#include "item.cpp"
-
+#include "game.h"
 
 void SnakeGame::game(){
-  while(true){
-    time_t start, end;
-    Head_Direction = 1;
-    set_zero();
-    color();
-    make_snake();
-    nodelay(stdscr, true); //delay 없이 움직이게 하기
     while(true){
-      // item, gate 초기화
-      for(int i=0;i<30;i++){
-        for(int j=0; j<30; j++){                                      //여기 바로 아래  '|| map[stage_num][i][j] == 8'추가함                              
-          if(map[stage_num][i][j] == 5 || map[stage_num][i][j] == 6 || map[stage_num][i][j] == 8)  map[stage_num][i][j] = 0;
-          if(map[stage_num][i][j] == 7 || map[stage_num][i][j] == 11) map[stage_num][i][j] = 1;
+        time_t start, end;
+        Head_Direction = 1;                  // alias 덕분에 그대로 동작
+        set_zero();
+        color();
+        snake_obj.make_snake();              // ★
+
+        nodelay(stdscr, true);
+        while(true){
+            for(int i=0;i<30;i++){
+                for(int j=0; j<30; j++){
+                    if(map[stage_num][i][j] == 5 || map[stage_num][i][j] == 6 || map[stage_num][i][j] == 8)
+                        map[stage_num][i][j] = 0;
+                    if(map[stage_num][i][j] == 7 || map[stage_num][i][j] == 11)
+                        map[stage_num][i][j] = 1;
+                }
+            }
+            gate.clear();
+            blue_gate.clear();
+            refresh();
+
+            generate_item();
+            generate_gate();
+            color();
+
+            start = time(NULL);
+            while(true){
+                Head_Direction = snake_obj.set_Head_Direction();   // ★
+                Head_Direction = snake_obj.move_Snake();           // ★
+                color();
+                current_speed_level = stage_num + 1;
+                score();
+                int base_delay = 400000;
+                int speed_up   = stage_num * 40000;
+                int final_delay = base_delay - speed_up;
+                if (final_delay < 80000) final_delay = 80000;
+                usleep(final_delay);
+                end = time(NULL);
+
+                if(end - start >= 15){
+                    stage_num++;
+                    stage_flag = 1;
+                    break;
+                }
+            }
+            if(stage_flag == 1) break;
         }
-      }
-      gate.clear();
-      blue_gate.clear();
-      refresh();
-
-      generate_item(); // item 생성
-      generate_gate(); // gate 생성
-      color();
-
-      start = time(NULL);
-while(true){
-  Head_Direction = set_Head_Direction();
-  Head_Direction = move_Snake();
-  color();
-  current_speed_level = stage_num + 1; // stage 0일 때 레벨 1, stage 3일 때 레벨 4
-  score(); // 이제 score() 함수가 실행될 때 바뀐 속도 레벨을 인식할 수 있습니다.
-  int base_delay = 400000;          // 기본 딜레이 (0.4초)로 증가시킴
-  int speed_up = stage_num * 40000; // 스테이지당 0.04초씩 딜레이 단축
-  int final_delay = base_delay - speed_up;
-
-  // 너무 빨라져서 게임이 터지는 걸 방지 (최소 딜레이 0.08초 보장)
-  if (final_delay < 80000) {
-      final_delay = 80000;
-   }
-  
-  usleep(final_delay); // 계산된 동적 딜레이 적용
-  end = time(NULL);
-  
-  if(end - start >= 15){
-    stage_num++;
-    stage_flag = 1;
-    break;
-  }
-}
-      if(stage_flag == 1){
-        break;
-      }
+        if(stage_num == 4){
+            NEXTGAME(2);
+            break;
+        }
+        NEXTGAME(1);
     }
-    if(stage_num == 4){
-      NEXTGAME(2);
-      break;
-    }
-    NEXTGAME(1);
-  }
 }
+
--- snake.cpp
+++ snake.cpp
@@ -1,38 +1,52 @@
-#include "game.h"
+#include "snake.h"
+#include "game.h"   // SnakeGame 정의, gate.h의 자유 함수도 함께 가져옴
+#include <cstdlib>
 
-// 스네이크 생성
-void SnakeGame::make_snake(){
-  int snake_location[4][3][2] = {
-    {{4, 23}, {6, 8}, {18, 16}},
-    {{15, 14}, {21, 9}, {3, 18}},
-    {{4, 15}, {27, 18}, {16, 15}},
-    {{16, 15}, {6, 12}, {27, 17}}
-  };
-
-  if(stage_num > 0){
-    for(int i=0; i<(int)snake.size()-1; i++){
-      map[stage_num][snake[i].y][snake[i].x] = 0;
-    }
-    snake.clear();
-    refresh();
-  }
-
-  srand((unsigned) time(0)); // 항상 다른 난수
-  int a = rand()%3;
-  for(int i=0; i<4; i++)
-    snake.push_back(snakepart(snake_location[stage_num][a][1]+i, snake_location[stage_num][a][0]));
-
-  map[stage_num][snake[0].y][snake[0].x] = 3;
-  for(int i=1; i<4; i++)
-    map[stage_num][snake[i].y][snake[i].x] = 4;
-  refresh();
-  color();
-  getch();
+Snake::Snake() {
+    Head_Direction = 1;
+    Body_length    = 0;
+    Growth_item    = 0;
+    Poison_item    = 0;
+    Reverse_active = 0;
+    Reverse_start  = 0;
+    key_to_dir[0] = 0;
+    key_to_dir[1] = 1;
+    key_to_dir[2] = 2;
+    key_to_dir[3] = 3;
+    game = nullptr;
 }
 
-//방향키 설정        ---------------전체 수정------
-int SnakeGame::set_Head_Direction() {
-    // 10초 경과 시 매핑 원복
+void Snake::make_snake(){
+    int snake_location[4][3][2] = {
+        {{4, 23}, {6, 8}, {18, 16}},
+        {{15, 14}, {21, 9}, {3, 18}},
+        {{4, 15}, {27, 18}, {16, 15}},
+        {{16, 15}, {6, 12}, {27, 17}}
+    };
+
+    if(game->stage_num > 0){
+        for(int i=0; i<(int)snake.size()-1; i++){
+            map[game->stage_num][snake[i].y][snake[i].x] = 0;
+        }
+        snake.clear();
+        refresh();
+    }
+
+    srand((unsigned) time(0));
+    int a = rand()%3;
+    for(int i=0; i<4; i++)
+        snake.push_back(snakepart(snake_location[game->stage_num][a][1]+i,
+                                  snake_location[game->stage_num][a][0]));
+
+    map[game->stage_num][snake[0].y][snake[0].x] = 3;
+    for(int i=1; i<4; i++)
+        map[game->stage_num][snake[i].y][snake[i].x] = 4;
+    refresh();
+    game->color();
+    getch();
+}
+
+int Snake::set_Head_Direction() {
     if (Reverse_active && time(NULL) - Reverse_start >= 10) {
         Reverse_active = 0;
         key_to_dir[0] = 0; key_to_dir[1] = 1;
@@ -47,143 +61,124 @@ int SnakeGame::set_Head_Direction() {
     case KEY_LEFT:  new_dir = key_to_dir[1]; break;
     case KEY_RIGHT: new_dir = key_to_dir[2]; break;
     case KEY_DOWN:  new_dir = key_to_dir[3]; break;
-    default: return Head_Direction;  // 키 입력 없음 → 그대로
+    default: return Head_Direction;
     }
 
-    // 반대방향 검사: 반대쌍은 합이 3 (0↔3, 1↔2)
     if (new_dir + Head_Direction == 3) {
-        NEXTGAME(3);
-        del_win();
+        game->NEXTGAME(3);
+        game->del_win();
         exit(0);
     }
 
     Head_Direction = new_dir;
     return Head_Direction;
-}//----------------수정 끝
-
-int SnakeGame::move_Snake(){
-  // 스네이크 다음 이동 위치
-  int move_posX = snake[0].x + head_way[Head_Direction][0];
-  int move_posY = snake[0].y + head_way[Head_Direction][1];
-
-  if(move_posX < 0 || move_posX >= 30 || move_posY < 0 || move_posY >= 30){
-    NEXTGAME(3);
-    del_win();
-    exit(0);
-  }
-
-  // growth item 먹을 때
-  if(map[stage_num][move_posY][move_posX] == 5){
-    // 머리 + 1
-    Growth_item += 1;
-    snake.insert(snake.begin(), snakepart(move_posX, move_posY));
-    map[stage_num][snake[0].y][snake[0].x] = 3;
-    map[stage_num][snake[1].y][snake[1].x] = 4;
-    refresh();
-  }
-
-  // poison item 먹을 때
-  else if(map[stage_num][move_posY][move_posX] == 6){
-    // 머리 + 1, 꼬리 - 2
-    if(snake.size() == 3){
-      NEXTGAME(3);
-      del_win();
-      exit(0);
-    }
-
-    Poison_item += 1;
-    map[stage_num][snake[snake.size()-1].y][snake[snake.size()-1].x] = 0;
-    map[stage_num][snake[snake.size()-2].y][snake[snake.size()-2].x] = 0;
-    snake.pop_back();
-    snake.pop_back();
-    snake.insert(snake.begin(), snakepart(move_posX, move_posY));
-    map[stage_num][snake[0].y][snake[0].x] = 3;
-    map[stage_num][snake[1].y][snake[1].x] = 4;
-    refresh();
-  }
-
-  // ── 추가: reverse direction item 먹을 때 ──
-  else if (map[stage_num][move_posY][move_posX] == 8) {
-      Reverse_active = 1;
-      Reverse_start = time(NULL);
-
-      // KEY_UP/LEFT/RIGHT 3개 키의 매핑을 셔플 (Fisher-Yates)
-      int dirs[3] = { 0, 1, 2 };
-      for (int i = 2; i > 0; i--) {
-          int j = rand() % (i + 1);
-          int t = dirs[i]; dirs[i] = dirs[j]; dirs[j] = t;
-      }
-      key_to_dir[0] = dirs[0];  // KEY_UP
-      key_to_dir[1] = dirs[1];  // KEY_LEFT
-      key_to_dir[2] = dirs[2];  // KEY_RIGHT
-      key_to_dir[3] = 3;        // KEY_DOWN은 그대로
-
-      // 일반 이동 (머리 +1, 꼬리 -1)
-      map[stage_num][snake[snake.size() - 1].y][snake[snake.size() - 1].x] = 0;
-      snake.pop_back();
-      snake.insert(snake.begin(), snakepart(move_posX, move_posY));
-      map[stage_num][snake[0].y][snake[0].x] = 3;
-      map[stage_num][snake[1].y][snake[1].x] = 4;
-      refresh();
-  }//  추가 끝
-
-  // 노란 gate를 만났을 때
-  else if(map[stage_num][move_posY][move_posX] == 7 && collision_gate() == 7){
-    Head_Direction = pass_the_gate();
-
-    gate_posX = get_yellow_exit_x() + head_way[Head_Direction][0];
-    gate_posY = get_yellow_exit_y() + head_way[Head_Direction][1];
-
-    map[stage_num][snake[snake.size()-1].y][snake[snake.size()-1].x] = 0;
-    snake.pop_back();
-    snake.insert(snake.begin(), snakepart(gate_posX, gate_posY));
-    map[stage_num][snake[0].y][snake[0].x] = 3;
-    map[stage_num][snake[1].y][snake[1].x] = 4;
-    refresh();
-  }
-
-  // 파란 gate를 만났을 때
-  else if(map[stage_num][move_posY][move_posX] == 11 && collision_gate() == 11){
-    Head_Direction = pass_the_blue_gate();
-
-    gate_posX = get_blue_exit_x() + head_way[Head_Direction][0];
-    gate_posY = get_blue_exit_y() + head_way[Head_Direction][1];
-
-    // body +1 효과: 꼬리를 pop하지 않음
-    snake.insert(snake.begin(), snakepart(gate_posX, gate_posY));
-    map[stage_num][snake[0].y][snake[0].x] = 3;
-    map[stage_num][snake[1].y][snake[1].x] = 4;
-
-    Growth_item += 1;
-    refresh();
-  }
-
-  //벽을 만났을 때
-  else if(map[stage_num][move_posY][move_posX] == 1 ||
-          map[stage_num][move_posY][move_posX] == 2 ||
-          map[stage_num][move_posY][move_posX] == 4){
-    NEXTGAME(3);
-    del_win();
-    exit(0);
-  }
-
-  // 아무것도 만나지 않을 때
-  else{
-    // 스네이크 전체가 gate를 다 통과했을 때
-    if(gate_posX != 0 && snake[snake.size()-1].x == gate_posX && snake[snake.size()-1].y == gate_posY){
-      finish_active_gate();
-      gate_posX = 0; gate_posY = 0;
-      refresh();
-    }
-
-    // 머리 + 1, 꼬리 - 1
-    map[stage_num][snake[snake.size()-1].y][snake[snake.size()-1].x] = 0;
-    snake.pop_back();
-    snake.insert(snake.begin(), snakepart(move_posX, move_posY));
-    map[stage_num][snake[0].y][snake[0].x] = 3;
-    map[stage_num][snake[1].y][snake[1].x] = 4;
-    refresh();
-  }
-
-  return Head_Direction;
 }
+
+int Snake::move_Snake(){
+    int stage_num = game->stage_num;
+    int move_posX = snake[0].x + head_way[Head_Direction][0];
+    int move_posY = snake[0].y + head_way[Head_Direction][1];
+
+    if(move_posX < 0 || move_posX >= 30 || move_posY < 0 || move_posY >= 30){
+        game->NEXTGAME(3);
+        game->del_win();
+        exit(0);
+    }
+
+    if(map[stage_num][move_posY][move_posX] == 5){
+        Growth_item += 1;
+        snake.insert(snake.begin(), snakepart(move_posX, move_posY));
+        map[stage_num][snake[0].y][snake[0].x] = 3;
+        map[stage_num][snake[1].y][snake[1].x] = 4;
+        refresh();
+    }
+    else if(map[stage_num][move_posY][move_posX] == 6){
+        if(snake.size() == 3){
+            game->NEXTGAME(3);
+            game->del_win();
+            exit(0);
+        }
+        Poison_item += 1;
+        map[stage_num][snake[snake.size()-1].y][snake[snake.size()-1].x] = 0;
+        map[stage_num][snake[snake.size()-2].y][snake[snake.size()-2].x] = 0;
+        snake.pop_back();
+        snake.pop_back();
+        snake.insert(snake.begin(), snakepart(move_posX, move_posY));
+        map[stage_num][snake[0].y][snake[0].x] = 3;
+        map[stage_num][snake[1].y][snake[1].x] = 4;
+        refresh();
+    }
+    else if (map[stage_num][move_posY][move_posX] == 8) {
+        Reverse_active = 1;
+        Reverse_start  = time(NULL);
+
+        int dirs[3] = { 0, 1, 2 };
+        for (int i = 2; i > 0; i--) {
+            int j = rand() % (i + 1);
+            int t = dirs[i]; dirs[i] = dirs[j]; dirs[j] = t;
+        }
+        key_to_dir[0] = dirs[0];
+        key_to_dir[1] = dirs[1];
+        key_to_dir[2] = dirs[2];
+        key_to_dir[3] = 3;
+
+        map[stage_num][snake[snake.size()-1].y][snake[snake.size()-1].x] = 0;
+        snake.pop_back();
+        snake.insert(snake.begin(), snakepart(move_posX, move_posY));
+        map[stage_num][snake[0].y][snake[0].x] = 3;
+        map[stage_num][snake[1].y][snake[1].x] = 4;
+        refresh();
+    }
+    // 노란 gate
+    else if(map[stage_num][move_posY][move_posX] == 7 && game->collision_gate() == 7){
+        Head_Direction = game->pass_the_gate();
+        game->gate_posX = get_yellow_exit_x() + head_way[Head_Direction][0];   // 자유 함수
+        game->gate_posY = get_yellow_exit_y() + head_way[Head_Direction][1];
+
+        map[stage_num][snake[snake.size()-1].y][snake[snake.size()-1].x] = 0;
+        snake.pop_back();
+        snake.insert(snake.begin(), snakepart(game->gate_posX, game->gate_posY));
+        map[stage_num][snake[0].y][snake[0].x] = 3;
+        map[stage_num][snake[1].y][snake[1].x] = 4;
+        refresh();
+    }
+    // 파란 gate
+    else if(map[stage_num][move_posY][move_posX] == 11 && game->collision_gate() == 11){
+        Head_Direction = game->pass_the_blue_gate();
+        game->gate_posX = get_blue_exit_x() + head_way[Head_Direction][0];     // 자유 함수
+        game->gate_posY = get_blue_exit_y() + head_way[Head_Direction][1];
+
+        snake.insert(snake.begin(), snakepart(game->gate_posX, game->gate_posY));
+        map[stage_num][snake[0].y][snake[0].x] = 3;
+        map[stage_num][snake[1].y][snake[1].x] = 4;
+
+        Growth_item += 1;
+        refresh();
+    }
+    else if(map[stage_num][move_posY][move_posX] == 1 ||
+            map[stage_num][move_posY][move_posX] == 2 ||
+            map[stage_num][move_posY][move_posX] == 4){
+        game->NEXTGAME(3);
+        game->del_win();
+        exit(0);
+    }
+    else{
+        if(game->gate_posX != 0 &&
+           snake[snake.size()-1].x == game->gate_posX &&
+           snake[snake.size()-1].y == game->gate_posY){
+            game->finish_active_gate();
+            game->gate_posX = 0;
+            game->gate_posY = 0;
+            refresh();
+        }
+        map[stage_num][snake[snake.size()-1].y][snake[snake.size()-1].x] = 0;
+        snake.pop_back();
+        snake.insert(snake.begin(), snakepart(move_posX, move_posY));
+        map[stage_num][snake[0].y][snake[0].x] = 3;
+        map[stage_num][snake[1].y][snake[1].x] = 4;
+        refresh();
+    }
+
+    return Head_Direction;
+}
+
